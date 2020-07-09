de1a78b06f56b896ca4303c45688ed82071699e5
yok <yokten10@gmail.com>
2020-07-09 21:21:19 +0700

    initial com

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+            print("YOK")
+
         }
     }
 
